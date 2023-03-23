81643b873abc70f85be718e978a45356cefbefd0
Devon Lewis <devonlewis_23@yahoo.com>
2023-03-23 10:03:14 -0500

    first dotnet app

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method='post' action='/helloworld/welcome'>" +
+                         "<input type='text' name='name' />" +
+                         "<input type='submit' value='Greet Me!' />" +
+                         "</form>";
+
+            return Content(html, "text/html");
+        }
+
+        // GET: /<controller>/welcome
+        [HttpGet]
+        [Route("helloworld/welcome/{name?}")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
---
+++ HelloASPDotNET/Controllers/welcome.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class welcome : Controller
+    {
+        public IActionResult Index()
+        {
+            string html = "<form method='post' action='/helloworld/welcome'>" +
+                "<input type='text' name='name' />" +
+                "<select name='language'><option value='english' selected>English</option>" +
+                "<option value='spanish'>Spanish</spanish>" +
+                "<option value='bosnian'>Bosnian</option>" +
+                "<option value='vietnamese'>Vietnamese</option>" +
+                "<option value='french'>French</option></select>" +
+                "<input type='submit' value='Greet Me!'/>" +
+                "</form>";
+
+            return Content(html, "text/html");
+        }
+    }
+}
